c4e2a34afffb46e90379ad313f6f1ee0fb841e23
jjyy032659 <z5133161@gmail.com>
2025-12-02 17:29:51 +1100

    add server-only code and third party packages

--- app/client-route/page.tsx
+++ app/client-route/page.tsx
@@ -1,11 +1,44 @@
 "use client";
 import { serverSideFunction } from "@/utils/server-utils";
 
+
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+
+
+
 export default function ClientRoutePage() {
-    const result = serverSideFunction();
-    console.log("This is a client-side component.");    
-        return <>
-        <h1>client Route Page</h1>;
-        <p>{result}</p>
-        </>
+
+const settings = {
+    dots: true,
+  };
+  return (
+    <div className="image-slider-container">
+      <Slider {...settings}>
+        <div>
+          <img src="http://placekitten.com/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/400/200" />
+        </div>
+        <div>
+          <img src="http://placekitten.com/400/200" />
+        </div>
+      </Slider>
+    </div>
+  );
+
+
+    // const result = serverSideFunction();
+    // console.log("This is a client-side component.");    
+    //     return <>
+    //     <h1>client Route Page</h1>;
+    //     <p>{result}</p>
+    //     </>
 }
--- app/server-route/page.tsx
+++ app/server-route/page.tsx
@@ -1,5 +1,8 @@
 import { serverSideFunction } from "@/utils/server-utils";
 
+
+
+
 export default function Page() {
 const result = serverSideFunction();
 
